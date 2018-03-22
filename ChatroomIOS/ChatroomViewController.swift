2405dd2edf4fccf62dd22fbe85532fa82aa686b8
jiaweitan <r9o0b@ugrad.cs.ubc.ca>
2018-03-22 14:23:15 -0700

    some change

---
+++ ChatroomIOS/ChatroomViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ChatroomViewController.swift
+//  chatroom
+//
+//  Created by Jiawei Tan on 2018-03-22.
+//  Copyright © 2018 Jiawei Tan. All rights reserved.
+//
+
+import UIKit
+
+class ChatroomViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+    @IBAction func Logout(_ sender: Any) {
+        self.performSegue(withIdentifier: "logout", sender: self)
+    }
+    @IBOutlet weak var outputArea: UITextView!
+    @IBOutlet weak var inputArea: UITextField!
+    
+    override func viewDidLoad() {
+        inputArea.delegate = self
+        outputArea.delegate = self
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == inputArea {
+            let text: String! = inputArea.text
+            if (!text.isEmpty) {
+                inputArea.text = ""
+                outputArea.text.append(text)
+                outputArea.text.append("\n")
+                print(text)
+            }
+        }
+        return true
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
